859a36e81374c7041e696e10ebdf7abe9642a3e4
LionZXY <nikita@kulikof.ru>
2022-04-29 20:01:05 +0400

    Remove const

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,7 +1,7 @@
 import java.lang.System.getProperty
 
 object Configuration {
-    const val version = getProperty("version", "debug")
-    const val repository = getProperty("group", "com.github.flipperdevices")
+    val version = getProperty("version", "debug")
+    val repository = getProperty("group", "com.github.flipperdevices")
     const val name = "protobuf-jvm"
 }
