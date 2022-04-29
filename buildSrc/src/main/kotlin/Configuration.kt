a5c3ff20088077bb52ce5ab7f54e4ffcd1869a7c
LionZXY <nikita@kulikof.ru>
2022-04-29 19:58:27 +0400

    Receive version and group from gradle command line

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,5 +1,7 @@
+import java.lang.System.getProperty
+
 object Configuration {
-    const val version = "0.6.1"
-    const val repository = "com.github.flipperdevices"
+    const val version = getProperty("version", "debug")
+    const val repository = getProperty("group", "com.github.flipperdevices")
     const val name = "protobuf-jvm"
 }
